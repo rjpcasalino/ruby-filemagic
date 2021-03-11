00bb35e13c4a62940ef9f50e6e5e74b2785f816e
rjpc <ryan@boringtranquility.io>
2021-03-11 13:17:51 -0800

    nix hack that I hope works :-)

--- ext/filemagic/extconf.rb
+++ ext/filemagic/extconf.rb
@@ -7,12 +7,12 @@ LIB_DIRS = [
 ]
 
 $CFLAGS << ' -Wall' if ENV['WALL']
-$LDFLAGS << ' -static-libgcc' if RUBY_PLATFORM =~ /cygwin|mingw|mswin/
+#$LDFLAGS << ' -static-libgcc' if RUBY_PLATFORM =~ /cygwin|mingw|mswin/
 
 #dir_config('magic', HEADER_DIRS, LIB_DIRS)
 #dir_config('gnurx', HEADER_DIRS, LIB_DIRS)
 
-have_library('gnurx')
+#have_library('gnurx')
 
 if have_library('magic', 'magic_open') && have_header('magic.h')
   have_func('magic_version')
